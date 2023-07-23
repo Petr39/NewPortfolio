b580662c967e34ba91f75c8ca1f1f8c74a603009
Petr Hanák <AccountPeter@seznam.cz>
2023-07-23 13:15:55 +0200

    zmeny

---
+++ NewPortfolio/Interfaces/IBuild.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewPortfolio.Models;
+
+
+namespace NewPortfolio.Interfaces
+ 
+
+
+{
+    public interface IBuild
+    {
+        Task<IEnumerable<Build>> ShowAll(int id);
+
+        void Create(Build build);
+
+        void Update(Build build);
+
+        void Details(int id);
+
+        void Delete(int id);
+
+        void Save();
+    }
+}
