77955fe061d63a202e174f14ca8c9e7cf0045f73
Petr Hanák <AccountPeter@seznam.cz>
2023-08-04 08:46:19 +0200

    layout ok

--- NewPortfolio/Models/Genre.cs
+++ NewPortfolio/Models/Genre.cs
@@ -7,6 +7,7 @@ namespace NewPortfolio.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name ="Žánr hry")]
         public string NameGenre { get; set; }
 
         public IEnumerable<Article>? Articles { get; set; }
